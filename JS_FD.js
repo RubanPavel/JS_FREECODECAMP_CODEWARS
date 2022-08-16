3be03b60a4b4e8da1f1b600ccb4a32b50ad4e4b4
Ruban Pavel <ruban.pavel@outlook.com>
2022-08-16 20:25:14 +0300

    3

--- JS_FD.js
+++ JS_FD.js
@@ -1,5 +1,7 @@
 /* Debugging */
 
+//1
+
 let a = 5;
 let b = 1;
 a++;
@@ -9,7 +11,16 @@ console.log(a);
 let sumAB = a + b;
 console.log(sumAB);
 
+//2
 let output =
   "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output);
 console.clear();
+
+//3
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
