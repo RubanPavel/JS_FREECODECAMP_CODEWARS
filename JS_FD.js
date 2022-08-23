080aeccc9dcfe59509c4c63676efe55854becb6d
Ruban Pavel <ruban.pavel@outlook.com>
2022-08-23 14:32:43 +0300

    add23.08

--- JS_FD.js
+++ JS_FD.js
@@ -242,14 +242,16 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 //7
 const increment = (number, value = 1) => number + value;
 
-//
+/* Basic Data Structures */
+
+//1
 let myArray = ["a", "b", "c", "d"];
 // Only change code below this line
 myArray[1] = "a";
 // Only change code above this line
 console.log(myArray);
 
-//
+//2
 function mixedNumbers(arr) {
   // Only change code below this line
   arr.unshift("I", 2, "three");
@@ -259,3 +261,35 @@ function mixedNumbers(arr) {
 }
 
 console.log(mixedNumbers(["IV", 5, "six"]));
+
+//3
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
+
+console.log(popShift(["challenge", "is", "not", "complete"]));
+//4
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+let sums = arr.splice(1, 4);
+// Only change code above this line
+console.log(arr);
+//5
+function htmlColorNames(arr) {
+  // Only change code below this line
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  // Only change code above this line
+  return arr;
+}
+
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
